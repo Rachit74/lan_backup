584bca65d49bb46b6ea41797fc0184420abca4ce
rachit74 <rachithooda09@gmail.com>
2026-01-19 13:38:20 +0530

    configs, tcp connection, ssh connection

--- src/bin/hasher.rs
+++
@@ -1,8 +0,0 @@
-
-use sha2::{Sha256, Digest};
-
-
-fn main() {
-    let result = Sha256::digest(b"file_name.txt");
-    println!("{:x}", result);
-}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
+use ssh2::Session;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::net::TcpStream;
+use std::path::{Path, PathBuf};
 use walkdir::WalkDir;
+use::std::env;
+use dotenv::dotenv;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    // load env
+    dotenv().ok();
+
+    // configs
+    let local_root = Path::new("/home/rachit/tool_test/src_dir");
+    let remote_root = Path::new("/home/rachit/tool_test/des_dir");
+
+    let host = "192.168.1.11:22";
+    let username = "rachit";
+    let password = env::var("PASS").expect("Password not found in env");
+
+    // tcp connection
+    let tcp = TcpStream::connect(host)?;
+
+    // ssh session
+    let mut session = Session::new()?;
+
+    session.set_tcp_stream(tcp);
+    session.handshake();
+
+    session.userauth_password(username, &password)?;
+
+    Ok(())
 
 
-fn main() {
-
-    for entry in WalkDir::new(".") {
-    let entry = entry.unwrap();
-    println!("{}", entry.path().display());
-}
 
 }
